b19423315f41c8227a21e9e5f5afec2d04723a51
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-08-03 19:11:50 -0400

    Basic logic completed

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("HELLO WORLD"));
+app.get("/", (req, res) => res.send("MAIN ROUTE"));
+
+app.get("/api/timestamp/:date_string?", (req, res) => {
+  const input = req.params.date_string;
+  let date;
+
+  if (input) {
+    date = new Date(input);
+  } else {
+    date = new Date();
+  }
+
+  if (date == "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
 
 app.listen(port, () => console.log(`Server runnig at port ` + port));
